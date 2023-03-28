88001cfa65e4844afdb65da77ac2920c25ec9241
codeninjascf <cnchandlersford@gmail.com>
2023-03-28 18:08:09 +0100

    update meany bird

--- Marc Beanie Bird/Assets/Scripts/PlayerControls.cs
+++ Marc Beanie Bird/Assets/Scripts/PlayerControls.cs
@@ -16,3 +16,13 @@ public class PlayerControls : MonoBehaviour
 
     }
 }
+
+//Game manager object
+[Header("Game Controller Object for controlling the game")]
+public GameController gameController;
+[Header("Default Velocity")]
+public float velocity = 1;
+//Physics for the bird
+private Rigidbody2D rb;
+//height of the bird object on the y axis
+private float objectHeight;
