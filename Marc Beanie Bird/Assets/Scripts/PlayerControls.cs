e2d5e5a6539a7e7365fe130ae9817ab45cb38ca6
codeninjascf <cnchandlersford@gmail.com>
2023-04-11 11:07:59 +0100

    a
    coding wee

--- Marc Beanie Bird/Assets/Scripts/PlayerControls.cs
+++ Marc Beanie Bird/Assets/Scripts/PlayerControls.cs
@@ -7,13 +7,25 @@ public class PlayerControls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        //Game Controller component
+        gameController = GetComponent<GameController>();
+        //Speed for the game is at playing state
+        Time.timeScale = 1;
+        rb = GetComponent<Rigidbody2D>();
+        //Object Height equals the size of the height of the sprite
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //If the left mouse button is clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            //The bird will float up on the y axis
+            //and float back down on Y axis
+            rb.velocity = Vector2.up * velocity;
+        }
     }
 }
 
@@ -21,7 +33,7 @@ public class PlayerControls : MonoBehaviour
 [Header("Game Controller Object for controlling the game")]
 public GameController gameController;
 [Header("Default Velocity")]
-public float velocity = 1;
+public float velocity = 5;
 //Physics for the bird
 private Rigidbody2D rb;
 //height of the bird object on the y axis
