1becb78201b7eea9eca3b62021ab8d805143f1be
codeninjascf <cnchandlersford@gmail.com>
2023-04-25 16:16:18 +0100

    ae

--- Marc Beanie Bird/Assets/Scripts/PlayerControls.cs
+++ Marc Beanie Bird/Assets/Scripts/PlayerControls.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    //Game manager object
+    [Header("Game Controller Object for controlling the game")]
+    public GameController gameController;
+    [Header("Default Velocity")]
+    public float velocity = 5;
+    //Physics for the bird
+    private Rigidbody2D rb;
+    //height of the bird object on the y axis
+    private float objectHeight;
+
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
         //Game Controller component
         gameController = GetComponent<GameController>();
         //Speed for the game is at playing state
@@ -15,16 +24,6 @@ public class PlayerControls : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         //Object Height equals the size of the height of the sprite
         objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
-=======
-
-        //Game Controller component
-        gameController = GetComponent<GameController>();
-        //Speed for the game is at a playing state
-        Time.timeScale = 1;
-        rb = GetComponent<Rigidbody2D>();
-        //Object Height equals the size of the height of the sprite
-        objectHeight = transform.GetComponents<SpriteRenderer>().bounds.size.y / 2;
->>>>>>> 383baaa7a217841ae386541d24e14bec35c40732
     }
 
     // Update is called once per frame
@@ -38,15 +37,6 @@ public class PlayerControls : MonoBehaviour
             rb.velocity = Vector2.up * velocity;
         }
     }
-    //Game manager object
-    [Header("Game Controller Object for controlling the game")]
-    public GameController gameController;
-    [Header("Default Velocity")]
-    public float velocity = 5;
-    //Physics for the bird
-    private Rigidbody2D rb;
-    //height of the bird object on the y axis
-    private float objectHeight;
 }
 
 
