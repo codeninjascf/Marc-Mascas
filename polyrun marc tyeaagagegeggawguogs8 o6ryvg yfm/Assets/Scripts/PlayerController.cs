397b21b66b54b2aa349da2a3695e1774b1b48603
codeninjascf <cnchandlersford@gmail.com>
2023-09-12 16:25:06 +0100

    help
    help

--- polyrun marc tyeaagagegeggawguogs8 o6ryvg yfm/Assets/Scripts/PlayerController.cs
+++ polyrun marc tyeaagagegeggawguogs8 o6ryvg yfm/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float jumpForce = 25f;
 
-    // Update is called once per frame
+    private bool _isGrounded;
+    private Rigidbody2D _rigidbody
+
+        void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
     void Update()
     {
-        
+        if (Input.GetMouseButton(0) && _isGrounded)
+        {
+            _rigidbody.velocity = Vector2.zero;
+            _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            if (PlayerCollisions.CollideWithSide(gameObject, other.gameObject, PlayerCollision.Side.Bottom))
+            {
+                _isGrounded = true;
+            }
+            else
+            {
+                GameManager.EndGame();
+            }
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            GameManager.EndGame();
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
 }
